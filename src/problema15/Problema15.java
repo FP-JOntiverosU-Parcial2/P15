bbd1bcaf481698c9b3dabcf2074a65baa0e1ceb5
jaime <jaime@vaio>
2016-10-20 23:55:01 -0500

    problema 15

---
+++ src/problema15/Problema15.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problema15;
+import java.util.Random;
+/**
+ *
+ * @author jaime
+ */
+public class Problema15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int [][] arr = new int [10][10];
+        int o;
+        operacion(arr);
+    }
+    public static int operacion(int [][] arr){
+        Random rnd = new Random();
+         for(int i = 0; i<=arr.length; i++)
+             for(int j = 0; j<arr.length; i++)
+     System.out.println(rnd.nextInt(50) + 1);
+           if (j==10){
+               
+           }
+        return arr;
+}
+}
+noTerminado ("D:")
+    
